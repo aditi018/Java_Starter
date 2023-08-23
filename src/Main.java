cd4db84d5e3b7753c01c5cb6d456213148be59b1
aditi018 <dasaditi018@gmail.com>
2023-08-23 23:14:33 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         DeluxPizza deluxPizza = new DeluxPizza(true);
+        Pizza basePizza = new Pizza(false);
+        basePizza.addExtraToppings();
+        basePizza.addExtraCheese();
+        basePizza.takeAway();
         deluxPizza.takeAway();
         deluxPizza.getBill();
+        basePizza.getBill();
     }
 }
